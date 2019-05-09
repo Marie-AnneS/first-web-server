313d442b1d0369acb5238de6b77ebbc795a30cc5
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-09 01:32:09 +0000

    ajout du login

--- express_server.js
+++ express_server.js
@@ -2,26 +2,28 @@ const http = require("http");
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
-var urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const deleteUrl = ( idUrl ) => {
+/* const loginId = user => {
+  if (user){
+    ret
+  }
+}; */
+
+const deleteUrl = idUrl => {
   //console.log(urlDatabase);
   delete urlDatabase[idUrl];
 }; //deleteUrl("9sm5xK"); console.log(urlDatabase);
-const exsitingKey = (key) => {
-  //for (const  in urlDatabase)
-}
-//route des pages
 
 app.post("/urls", (req, res) => {
   urlDatabase["new_url"] = req.body.longURL; // Log the POST request body to the console
@@ -29,38 +31,69 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
 });
 
+//Do i need /logout
+app.post("/logout", (req, res) => {
+  const user = req.body.username;
+  res.clearCookie("username", user);
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  const user = req.body.username;
+  res.cookie("username", user);
+/*   let putUser = () => {res.cookie("username", user);};   
+  let ereaseUser = () => {res.clearCookie("username", user);};
+  user ? putUser() : ereaseUser() ; */
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL", (req, res) => {
   const shortUrl = req.params.shortURL;
   urlDatabase[shortUrl] = req.body.newUrl; // Log the POST request body to the console
-
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortUrl = req.params.shortURL;
-  //let templateVars = { urls: urlDatabase };
   deleteUrl(shortUrl);
-  //res.render("urls_index", templateVars);
+  //console.log(shortUrl);
   res.redirect("/urls");
 });
 
+//    -------------  Mes routes des pages   ---------
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortUrl = req.params.shortURL;
+  let longURL = urlDatabase[shortUrl];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.post("/login", (req, res) => {
-  const user = res.cookie;
-  console.log(user);
-  res.render("urls_new");
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/urls/:shortURL", (req, res) => {
+  const shortUrl = req.params.shortURL;
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: shortUrl,
+    longURL: urlDatabase[shortUrl]
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
@@ -71,13 +104,6 @@ app.get("/hello", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/urls/:shortURL", (req, res) => {
-  const shortUrl = req.params.shortURL;
-  let templateVars = { shortURL: shortUrl, longURL: urlDatabase[shortUrl] };
-  res.render("urls_show", templateVars);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
