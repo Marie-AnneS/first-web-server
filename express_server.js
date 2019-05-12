7092cfce0a316363a05e57ae2b29613b6316a0d2
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-12 18:31:08 +0000

    clean up the required and finction

--- express_server.js
+++ express_server.js
@@ -1,16 +1,21 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser"); // !!! a enleve
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 const express = require("express");
 const http = require("http");
 
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.listen(PORT, () => {});
+app.use(cookieSession({
+  name: 'session',
+  keys: ["secretkeys"]
+}))
 
 //  --------- DATABASE  -------------------------  //
 
@@ -24,13 +29,13 @@ const urlDatabase = {
 const userDB = {
   "aJ48lW": {
     id: "aJ48lW",
-    email: "marie.anne.seim@gmail.com",
-    password: "poire"
+    email: "t1@t1.com",
+    password: bcrypt.hashSync("t1", 10)
   },
   "22sd2": {
-    id: "22sd22",
-    email: "anne.seim@gmail.com",
-    password: "anne"
+    id: "22sd2",
+    email: "t2@22.com",
+    password: bcrypt.hashSync("t1", 10)
   }
 };
 
@@ -50,7 +55,7 @@ const findUserId = (user_id_bd, user_id) => {
   }
   return false;
 };
-
+//!!! enlever
 const findEmail = (dB, email) => {
   for (const key in dB) {
     if (dB[key].email === email) {
@@ -59,7 +64,15 @@ const findEmail = (dB, email) => {
   }
   return false;
 };
-//    funtion Optionel
+
+const findUserByEmail = (dB, email) => {
+  for (const key in dB) {
+    if (dB[key].email === email) {
+      return key;
+    }
+  }
+  return false;
+};
 
 /* This one don't work's
 const urlsForUser = (id) => {
@@ -74,46 +87,44 @@ const urlsForUser = (id) => {
   return arrUrlfromShort;
 }; */
 
-const dbForUser = (id) => {
-  var copieurUrlDatabase = Object.assign({}, urlDatabase); 
-  var userId = findUserId(copieurUrlDatabase, id)
-   for (const key in copieurUrlDatabase) {
-    if (copieurUrlDatabase[key].userID !== id){
-      copieurUrlDatabase
-      delete copieurUrlDatabase[key];
+//    funtion Optionel
+
+const dbForUser = id => {
+  var copie_UrlDatabase = Object.assign({}, urlDatabase);
+  // var userId = findUserId(copie_UrlDatabase, id)
+  for (const key in copie_UrlDatabase) {
+    if (copie_UrlDatabase[key].userID !== id) {
+      copie_UrlDatabase;
+      delete copie_UrlDatabase[key];
     }
-  } 
-  return copieurUrlDatabase;
+  }
+  return copie_UrlDatabase;
 };
-
-
 
 const addNewUser = (email, password) => {
-  const idGenerate = generateRandomString();
-  const userCript = {
-    id: idGenerate,
-    email: email,
-    password: password
+  const id = generateRandomString();
+  const obj_id = {
+    id,
+    email,
+    password: bcrypt.hashSync(password, 10)
   };
 
-  userDB[idGenerate] = userCript;
-  return userCript;
+  userDB[id] = obj_id;
+  return obj_id;
 };
-
+//!!! pas utilise
 const addNewObjUrl = (longUrl, user_id) => {
   const NewObjURL = {
     longUrl: longUrl,
     user_id: user_id
   };
   return NewObjURL;
-}
-
+};
 
 const authentif = function(email, passwordpm) {
-  
-  var userId =  findEmail(userDB, email)
+  var userId = findUserByEmail(userDB, email);
   if (userId && bcrypt.compareSync(passwordpm, userDB[userId].password)) {
-      return true;
+    return true;
   }
 };
 
@@ -121,38 +132,108 @@ const deleteUrl = idUrl => {
   delete urlDatabase[idUrl];
 }; //deleteUrl("9sm5xK"); console.log(urlDatabase);
 
+//-------------GET  Mes routes des pages   ---------//
+
+app.get("/", (req, res) => {
+  /* if (user_id){
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  } */
+  res.redirect("/urls");
+});
+
+app.get("/urls", (req, res) => {
+  const usertest = req.cookies.user_id;
+  console.log(usertest);
+  let templateVars = {
+    findUserId: findUserId(urlDatabase, req.cookies.user_id),
+    longURL: urlDatabase[req.cookies.user_id],
+    obsUser: userDB[req.cookies.user_id],
+    shortURL: req.params.shortURL,
+    urlDatabase: urlDatabase,
+    urlsForUser: dbForUser(req.cookies.user_id), //urlsForUser
+    user_email: req.cookies["email"],
+    user_id: req.cookies.user_id,
+    user_password: req.cookies["password"],
+    userDB: userDB
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = {
+    obsUser: userDB[req.cookies.user_id],
+    user_id: req.cookies.user_id,
+    urls: dbForUser(req.cookies.user_id)
+  };
+  if (templateVars.user_id) {
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    objURL: urlDatabase[req.params.shortURL],
+    obsUser: userDB[req.cookies.user_id],
+    shortURL: req.params.shortURL,
+    urls: dbForUser(req.cookies.user_id),
+    user_email: req.cookies["email"],
+    user_id: req.cookies.user_id,
+    user_password: req.cookies["password"],
+    userDB: userDB
+  };
+
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortUrl = req.params.shortURL;
+  const longURL = urlDatabase[shortUrl].longURL;
+
+  res.redirect(longURL);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user_id: req.cookies.user_id,
+    userDB: userDB,
+    obsUser: userDB[req.cookies.user_id]
+  };
+  res.render("login", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user_id: req.cookies.user_id,
+    userDB: userDB,
+    obsUser: userDB[req.cookies.user_id]
+  };
+  res.render("register", templateVars);
+});
+
+// !!! avoir s'il faut garder
+app.get("/logout", (req, res) => {
+  let templateVars = {
+    user_id: req.cookies.user_id,
+    user_email: req.cookies["email"],
+    user_password: req.cookies["password"],
+    userDB: userDB
+  };
+  res.render("urls_index", templateVars);
+});
+
 //-----------   POST  ---------------//
 
 app.post("/urls", (req, res) => {
   const ramdomShortUrl = generateRandomString();
   const LongUrl = req.body.longURL;
-  const user = req.cookies["user_id"];  
-  urlDatabase[ramdomShortUrl] = { longURL: LongUrl, userID: user }  
+  const user = req.cookies.user_id;
+
+  urlDatabase[ramdomShortUrl] = { longURL: LongUrl, userID: user };
   res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
-});
-
-app.post("/logout", (req, res) => {
-  const user = req.cookies["user_id"];
-  const email = userDB[user].email;
-  const password = userDB[user].password;
-  res.clearCookie("user_id", user);
-  res.clearCookie("user_email", email);
-  res.clearCookie("user_password", password);
-  res.redirect("/urls");
-});
-
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  let user_id = findEmail(userDB, email);
-
-  if (authentif(email, password)) {
-    res.cookie("user_id", user_id);
-    res.cookie("user_email", email);
-    res.cookie("user_password", password);
-    res.redirect("/urls");
-  } else {
-    res.status(403).send("Votre email ou passe]word n'est pas le bon");
-  }
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -167,10 +248,24 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  let user_id = findUserByEmail(userDB, email);
+
+  if (authentif(email, password)) {
+    res.cookie("user_id", user_id);
+    res.cookie("user_email", email);
+    res.cookie("user_password", password);
+    res.redirect("/urls");
+  } else {
+    res.status(403).send("Votre email ou passe]word n'est pas le bon");
+  }
+});
+
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
-  const criptPassword = bcrypt.hashSync(password, 10); 
-  let user_id = findEmail(userDB, email);
+  const criptPassword = bcrypt.hashSync(password, 10);
+  let user_id = findUserByEmail(userDB, email);
 
   if (user_id || !email || !password) {
     res.status(400).send("Votre Email ou Password existe deja");
@@ -183,101 +278,12 @@ app.post("/register", (req, res) => {
   }
 });
 
-//-------------GET  Mes routes des pages   ---------//
-
-app.get("/urls", (req, res) => {
-  let templateVars = {
-    findUserId: findUserId(urlDatabase ,req.cookies["user_id"]),
-    longURL: urlDatabase[req.cookies["user_id"]],
-    obsUser: userDB[req.cookies["user_id"]],
-    shortURL: req.params.shortURL,
-    urlDatabase: urlDatabase,
-    urlsForUser: dbForUser(req.cookies["user_id"]), //urlsForUser
-    user_email: req.cookies["email"],
-    user_id: req.cookies["user_id"],
-    user_password: req.cookies["password"],
-    userDB: userDB,
-  };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const shortUrl = req.params.shortURL;
-  const longURL = urlDatabase[shortUrl].longURL;
- 
-  res.redirect(longURL);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    obsUser: userDB[req.cookies["user_id"]],
-    user_id: req.cookies["user_id"],
-    urls: dbForUser(req.cookies["user_id"]),
-  };
-  if (templateVars.user_id) {
-    res.render("urls_new", templateVars);
-  } else {
-    res.redirect("/login");
-  }
-  
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {
-    objURL: urlDatabase[req.params.shortURL],
-    obsUser: userDB[req.cookies["user_id"]],
-    shortURL: req.params.shortURL,
-    urls: dbForUser(req.cookies["user_id"]),
-    user_email: req.cookies["email"],
-    user_id: req.cookies["user_id"],
-    user_password: req.cookies["password"],
-    userDB: userDB,
-  };
-
-  res.render("urls_show", templateVars);
-});
-
-app.get("/register", (req, res) => {
-  let templateVars = {
-    user_id: req.cookies["user_id"],
-    userDB: userDB,
-    obsUser: userDB[req.cookies["user_id"]]
-  };
-  res.render("register", templateVars);
-});
-
-app.get("/login", (req, res) => {
-  let templateVars = {
-    user_id: req.cookies["user_id"],
-    userDB: userDB,
-    obsUser: userDB[req.cookies["user_id"]]
-  };
-  res.render("login", templateVars);
-});
-
-app.get("/logout", (req, res) => {
-  let templateVars = {
-    user_id: req.cookies["user_id"],
-    user_email: req.cookies["email"],
-    user_password: req.cookies["password"],
-    userDB: userDB
-  };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.post("/logout", (req, res) => {
+  const user = req.cookies.user_id;
+  const email = userDB[user].email;
+  const password = userDB[user].password;
+  res.clearCookie("user_id", user);
+  res.clearCookie("user_email", email);
+  res.clearCookie("user_password", password);
+  res.redirect("/urls");
 });
